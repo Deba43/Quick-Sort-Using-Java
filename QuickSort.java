4f228f6d3698388f01a48529ac368efdefa265ca
Debadatta Behera <105921307+Deba43@users.noreply.github.com>
2024-01-26 22:08:46 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -29,7 +29,7 @@ public class QuickSort {
                 array[j] = temp;
             }
         }
-        i++;
+                i++;
                 int temp = array[i];
                 array[i] = array[end];
                 array[end] = temp;
